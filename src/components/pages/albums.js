1397845045255b5cd70824af5f047e303236efa7
Oliver K. Plummer <oly.rbsc@gmail.com>
2017-04-14 08:40:14 +1000

    Moved album helper into source file;

--- src/components/pages/albums.js
+++ src/components/pages/albums.js
@@ -1,8 +1,37 @@
 import React from 'react'
-import renderAlbums from '../helpers/albums-helper'
 import artistsObj from '../../data/library/artists'
 import albumsObj from '../../data/library/albums'
 import './albums.css'
+
+const renderAlbums = (albums, artists) => {
+	let length = albums.length
+	return albums.map((album, index) => {
+
+		let artist, i = 0
+
+		for (i; i < length; i++) {
+			let item = artists[i]
+			if (item.artistId === album.artistId) {
+				artist = item
+				break
+			}
+		}
+
+		return (
+			<div
+				className="album"
+				key={index} >
+				<div className="album-inner">
+					<img src={album.cover} alt={album.cover} />
+					<div className="album-text">
+						<h3>{album.title}</h3>
+						<h4>{artist.name}</h4>
+					</div>
+				</div>
+			</div>
+		)
+	})
+}
 
 const Albums = () => {
 	return (
