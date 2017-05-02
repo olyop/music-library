db280c937464d29cd5cb3659ad8532aa69123070
Oliver <oly.rbsc@gmail.com>
2017-05-02 13:05:06 +1000

    Added play bar

--- src/components/library.js
+++ src/components/library.js
@@ -8,59 +8,66 @@ import Home from './pages/home'
 import Artists from './pages/artists'
 import Albums from './pages/albums/albums'
 import Songs from './pages/songs'
+import Play from './pages/play'
 
 const Library = props => (
 	<Router>
-		<div className="container-fluid library">
-			<div className="row">
+		<div>
+		
+			<Play />
+			
+			<div className="container-fluid library">
+				<div className="row">
 
-				{ props.isNav ? <SideBar info={props.info} /> : null }
+					{ props.isNav ? <SideBar info={props.info} /> : null }
 
-				<div className={props.isNav ? 'col-md-10 content' : 'col-md-12 content'}>
+					<div className={props.isNav ? 'col-md-10 content' : 'col-md-12 content'}>
 
-					<Route exact
-						path="/"
-						render={ () => (
+						<Route exact
+							path="/"
+							render={ () => (
 
-							<Home
-								info={props.info}
-								mainObj={props.mainObj} />
+								<Home
+									info={props.info}
+									mainObj={props.mainObj} />
 
-						)} />
+							)} />
 
-					<Route exact
-						path="/artists"
-						render={ () => (
+						<Route exact
+							path="/artists"
+							render={ () => (
 
-							<Artists
-								info={props.info}
-								mainObj={props.mainObj} />
+								<Artists
+									info={props.info}
+									mainObj={props.mainObj} />
 
-						)} />
+							)} />
 
-					<Route exact
-						path="/albums"
-						render={ () => (
+						<Route exact
+							path="/albums"
+							render={ () => (
 
-							<Albums
-								info={props.info}
-								mainObj={props.mainObj} />
+								<Albums
+									info={props.info}
+									mainObj={props.mainObj} />
 
-						)} />
+							)} />
 
-					<Route exact
-						path="/songs"
-						render={ () => (
+						<Route exact
+							path="/songs"
+							render={ () => (
 
-							<Songs
-								info={props.info}
-								mainObj={props.mainObj} />
+								<Songs
+									info={props.info}
+									mainObj={props.mainObj} />
 
-						)} />
+							)} />
+
+					</div>
 
 				</div>
-
 			</div>
+		
 		</div>
 	</Router>
 )
---
+++ src/components/pages/play.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import './play.css'
+
+class Play extends React.Component {
+	
+	constructor(props) {
+		super(props)
+		
+		this.state = { isPlay: false }
+		this.onPlayClick = this.onPlayClick.bind(this)
+	}
+	
+	onPlayClick() {
+		this.setState(prevState => ({
+			isPlay: !prevState.isPlay
+		}))
+	}
+	
+	render() {
+		return (
+			<div className="play">
+				
+				<div className="play-section play-left">
+					<img src="./img/album-covers/thriller.png" alt="Album" />
+					<div className="play-content">
+						<div>
+							<h1>Thriller</h1>
+							<p>Micheal Jackson</p>
+						</div>
+					</div>
+				</div>
+				
+				<div className="play-section play-middle">
+					<i className="material-icons">repeat</i>
+					<i className="material-icons">skip_previous</i>
+					<i className="material-icons play-play-button"
+						onClick={this.onPlayClick}>
+						{this.state.isPlay ? 'play_circle_filled' : 'pause_circle_filled'}
+					</i>
+					<i className="material-icons">skip_next</i>
+					<i className="material-icons">shuffle</i>
+				</div>
+				
+				<div className="play-section play-right"></div>
+				
+			</div>
+		)
+	}
+}
+
+export default Play
