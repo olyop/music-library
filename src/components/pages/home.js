26b19b16019f2b7f7db6eb4f4252293b07287fdb
Oliver <oly.rbsc@gmail.com>
2017-05-23 12:25:33 +1000

    Update

--- src/components/pages/home.js
+++ src/components/pages/home.js
@@ -4,7 +4,7 @@ const Home = props => {
 	return (
 		<div>
 			<h1>Home</h1>
-			<div>{JSON.stringify(props.mainObj)}</div>
+			<div>{JSON.stringify(props.mainObj, null, "\t")}</div>
 		</div>
 	)
 }
